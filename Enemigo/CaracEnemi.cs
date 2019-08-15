cf1176ec111177bf4f29b3841f174687ea07ab7f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-15 07:45:20 -0700

    Agregar archivos de proyecto.

---
+++ Enemigo/CaracEnemi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enemigo
+{
+    class CaracEnemi
+    {
+        public int posicionx { get; set; }
+        public int posiciony { get; set; }
+
+        private int _puntos;
+        public int Puntos
+        {
+            get
+            {
+                return _puntos;
+            }
+            set
+            {
+                if(value >= 100)
+                {
+                    _puntos = value;
+                }
+                else
+                {
+                    throw new Exception("no gano puntos");
+                }
+            }
+        }
+        public void moverse() { }
+        public void disparar() { }
+        public void explotar() { }
+
+
+
+
+
+    }
+}
